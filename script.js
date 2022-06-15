67667b29ece4f8b4b8f53a26647f88f616cc27b0
Alfredo <alfredofontinele08@gmail.com>
2022-06-15 16:59:36 -0300

    update

--- script.js
+++ script.js
@@ -1,63 +1,37 @@
-// Tabela
+const qtd_Refrigerante = parseInt(prompt("Digite a quantidade de refrigerante que você comprou:"));
+const valorRefrigerante = parseFloat(prompt("Digite o valor unitário do refrigerante:"));
+const totalRefrigerante = (qtd_Refrigerante * valorRefrigerante);
 
-// Produto	        Quantidade	Valor Unitário
-// Refrigerante	        3           11,99
-// Macarrão	            4	        6,99
-// Ervilha	            3	        6,99
-// Arroz	            3	        22,99
-// Feijão	            2	        11,89
-// Vinho	            3	        70,00
+const qtd_Macarrao = parseInt(prompt("Digite a quantidade de macarrão que você comprou:"));
+const valorMacarrao = parseFloat(prompt("Digite o valor unitário do macarrão:"));
+const totalMacarrao = (qtd_Refrigerante * valorRefrigerante);
 
-// Multiplique o valor de cada item pela quantidade e atribua 
-// a uma variável (Cada total em uma variável diferente).
+const qtd_Ervilha = parseInt(prompt("Digite a quantidade de Ervilha que você comprou:"));
+const valorErvilha = parseFloat(prompt("Digite o valor unitário da Ervilha:"));
+const totalErvilha = (qtd_Ervilha * valorErvilha);
 
-// Some as variáveis e atribua a uma nova variável "total".
-// Teste se a variável "total" é ímpar ou par.
+const qtd_Arroz = parseInt(prompt("Digite a qtd_ do Arroz que você comprou:"));
+const valorArroz = parseFloat(prompt("Digite o valor unitário do Arroz:"));
+const totalArroz = (qtd_Arroz * valorArroz);
 
-// Se a variável "total" for ímpar, cada amigo deverá pagar metade do 
-// valor total da compra, incluindo o vinho.
+const qtd_Feijao = parseInt(prompt("Digite a quantidade de Feijão que você comprou:"));
+const valorFeijao = parseFloat(prompt("Digite o valor unitário do Feijão:"));
+const totalFeijao = (qtd_Feijao * valorFeijao);
 
-// Se a variável "total" for par você pagará somente a metade e seu amigo
-// pagará a metade mais o valor do vinho.
+const qtd_Vinho = parseInt(prompt("Digite a quantidade de Vinho que você comprou:"));
+const valorVinho = parseFloat(prompt("Digite o valor unitário do Vinho:"));
+const totalVinho = (qtd_Vinho * valorVinho);  
 
-const qtd_refrigerante = parseInt(prompt("Digite a quantidade de Refrigerante: "));
-const preco_refrigerante = parseFloat(prompt("Digite o preço por unidade do Refrigerante: "));
+const total = (totalRefrigerante + totalMacarrao + totalErvilha + totalArroz + totalFeijao + totalVinho);
+const totalSemVinho = (totalRefrigerante + totalMacarrao + totalErvilha + totalArroz + totalFeijao);
 
-const qtd_macarrao = parseInt(prompt("Digite a quantidade de Macarrão: "));
-const preco_macarrao = parseFloat(prompt("Digite o preço por unidade do Macarrão: "));
-
-const qtd_ervilha = parseInt(prompt("Digite a quantidade de Ervilha: "));
-const preco_ervilha = parseFloat(prompt("Digite o preço por unidade do Ervilha: "));
-
-const qtd_arroz = parseInt(prompt("Digite a quantidade de Arroz: "));
-const preco_arroz = parseFloat(prompt("Digite o preço por unidade do Arroz: "));
-
-const qtd_feijao = parseInt(prompt("Digite a quantidade de Feijão: "));
-const preco_feijao = parseFloat(prompt("Digite o preço por unidade do Feijão: "));
-
-const qtd_vinho = parseInt(prompt("Digite a quantidade de Vinho: "));
-const preco_vinho = parseFloat(prompt("Digite o preço por unidade do Vinho: "));
-
-const total = (
-    (qtd_refrigerante * preco_refrigerante) + 
-    (qtd_macarrao * preco_macarrao) + 
-    (qtd_ervilha * preco_ervilha) + 
-    (qtd_arroz * preco_arroz) + 
-    (qtd_feijao * preco_feijao) + 
-    (qtd_vinho * preco_vinho)
-);
-
-const vinho = qtd_vinho * preco_vinho;
-const metade = total / 2;
-
-if (total % 2 == 0) {
-    let voce = metade - vinho;
-    let amigo = metade + vinho;
-    alert(`Valor Total: R$${total} (Par)\nVocê vai pagar: R$${voce}\nSeu amigo vai pagar: R$${amigo}`);
-    console.log(`Valor Total: R$${total}\nVocê vai pagar: R$${voce}\nSeu amigo vai pagar: R$${amigo}`);
+if (total % 2 === 0) {
+    let resultado = totalSemVinho.toFixed(2) / 2 + totalVinho;
+    let metade = totalSemVinho.toFixed(2) / 2;
+    console.log (`Valor Total => R$${total.toFixed(2)} (Par)\n\nVocê deverá pagar R$${metade}\nE seu amigo deve => R$${resultado}`);
+    alert (`Valor Total => R$${total.toFixed(2)} (Par)\n\nVocê deverá pagar R$${metade}\nE seu amigo deve => R$${resultado}`);
 } else {
-    let voce = metade;
-    let amigo = metade;
-    alert(`Valor Total: R$${total} (Ímpar)\nVocê vai pagar: R$${voce}\nSeu amigo vai pagar: R$${amigo}`);
-    console.log(`Valor Total: R$${total}\nVocê vai pagar: R$${voce}\nSeu amigo vai pagar: R$${amigo}`);
+    let metade = total.toFixed(2) / 2;
+    console.log (`Valor Total => R$${total.toFixed(2)} (Ímpar)\n\nVocês vão dividir o valor total de vinho que é de R$${totalVinho}\nAmbos vão pagar => R$${metade}`);
+    alert (`Valor Total => R$${total.toFixed(2)} (Ímpar)\n\nVocês vão dividir o valor total de vinho que é de R$${totalVinho}\nAmbos vão pagar => R$${metade}`);
 }
